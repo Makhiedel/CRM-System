bb6f1747e3a29a6846b4d7a87b2979c9a28cc78a
Makhiedel <muwwwwwwwww@gmail.com>
2026-06-03 18:43:16 +0300

    antd form validation changes

--- src/components/AddTask/AddTask.tsx
+++ src/components/AddTask/AddTask.tsx
@@ -13,50 +13,63 @@ interface Props {
 }
 
 export default function AddTask({ handleUpdate }: Props) {
+  const [form] = Form.useForm();
   const [taskName, setTaskName] = useState<string>("");
   const [validation, setValidation] = useState<Validator>({ isValid: true });
 
   function handleTaskName(event: React.ChangeEvent<HTMLInputElement>): void {
     setTaskName(event.target.value);
-    setValidation({ isValid: true });
+    // setValidation({ isValid: true });
   }
 
-  async function setSubmit(): Promise<void> {
-    const taskData: TaskData = { isDone: false, title: taskName };
-
-    if (validate(taskName).isValid) {
-      try {
-        const response = await createTask(taskData);
-        console.log(`"${taskName}" task created`, response);
-        setTaskName(""); //clear input only if task is created
-      } catch (error) {
-        if (axios.isAxiosError(error)) {
-          console.error("HTTP error", error.response?.status, error.message);
-        } else {
-          console.log(`Failed to create task! ${error}`);
-        }
+  async function setSubmit(value): Promise<void> {
+    const taskData: TaskData = { isDone: false, title: value };
+    console.log(value);
+    try {
+      const response = await createTask(taskData);
+      console.log(`"${value}" task created`, response);
+   
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error("HTTP error", error.response?.status, error.message);
+      } else {
+        console.log(`Failed to create task! ${error}`);
       }
-      handleUpdate();
-    } else {
-      setValidation(validate(taskName));
-
-      setTaskName("");
     }
+    handleUpdate();
+    form.resetFields();
+    // if (validate(taskName).isValid) {
+    // } else {
+    //   setValidation(validate(taskName));
+
+    //   setTaskName("");
+    // }
   }
 
   return (
     <div className={styles.taskcreator}>
-      <div className={styles.taskcreatorrow}>
-          <Input
-            onChange={handleTaskName}
-            type="text"
-            placeholder="Task to be done..."
-            value={taskName}
-          />
-        <Button onClick={setSubmit} type="primary" size="large">
-          Add
-        </Button>
-      </div>
+      <Form
+        form={form}
+        name="addTask"
+        className={styles.taskcreatorrow}
+        onFinish={setSubmit}
+      >
+        <Form.Item
+          rules={[
+            { required: true, message: "Please enter the title!" },
+            { min: 3, message: "Minimum 3 characters" },
+            { max: 64, message: "Maximum 64 characters" },
+          ]}
+        >
+          <Input placeholder="Task to be done..." />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" size="large" htmlType="submit">
+            Add
+          </Button>
+        </Form.Item>
+      </Form>
+
       {!validation.isValid && (
         <p className={styles.errortext}>{validation.errorMessage}</p>
       )}
