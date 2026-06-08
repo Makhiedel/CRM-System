81ad190c3c752fcb20f8b227d0c97bdb4db19aa8
Makhiedel <muwwwwwwwww@gmail.com>
2026-06-08 23:23:00 +0300

    SideMenu upgrade

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
-import SideMenu from "./components/SideMenu/SideMenu"
+import Menu from "./pages/Menu"
 import "./App.css";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/profile", element: <ProfilePage /> },
+  {
+    path: "/",
+    element: <Menu />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/profile", element: <ProfilePage /> },
+    ]
+  },
 ]);
 
 export default function App() {
   return (
-    <>
-      <SideMenu />
       <RouterProvider router={router} />
-    </>
   );
 }
--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -1,11 +1,18 @@
 import { Button } from "antd";
+import { useNavigate } from "react-router-dom";
 import styles from "./SideMenu.module.css";
 
 export default function SideMenu() {
+  const navigate = useNavigate();
+  
+  function navigateHandler(url:"/"|"/profile"):void {
+    navigate(url);
+  }
+
   return (
     <div className={styles.sidemenucontainer}>
-      <Button size="large">Список задач</Button>
-      <Button size="large">Профиль</Button>
+      <Button onClick={()=>navigateHandler("/")}>Список задач</Button>
+      <Button onClick={()=>navigateHandler("/profile")}>Профиль</Button>
     </div>
   )
 }
