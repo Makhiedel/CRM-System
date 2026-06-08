f9cdb30636a0b6caf5f41fe6c11fd32b7633ac0e
Makhiedel <muwwwwwwwww@gmail.com>
2026-06-08 12:56:01 +0300

    react-router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import HomePage from "./pages/HomePage";
 import "./App.css";
 
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage />},
+]);
+
 export default function App() {
   return (
-    <HomePage />
+    <RouterProvider router={router} />
   )
 }
